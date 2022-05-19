ef43673b3e1a230373a657b9d746bd53c3ee256b
Lucas Tinoco <lucasltinoco@gmail.com>
2022-05-19 18:48:56 -0300

    adiciona bot feliz

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 #encoding: utf-8
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
+from Bots.BotFeliz import BotFeliz
 #adicionar mais bots
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Eduardo")]
+lista_bots = [BotZangado("Eduardo"), BotFeliz("Lucas")]
 
 sys = scb.SistemaChatBot("BotsGP1",lista_bots)
 sys.inicio()
